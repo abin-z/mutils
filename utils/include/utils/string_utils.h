a2f89641479125f407e32a634fd7820e6026739b
abin <abin.z@outlook.com>
2026-01-22 23:46:02 +0800

    feat: 添加函数重载

--- utils/include/utils/string_utils.h
+++ utils/include/utils/string_utils.h
@@ -17,6 +17,8 @@
 namespace stringutils
 {
 std::vector<std::string> split(const std::string &str, char delimiter);
+std::vector<std::string> split(const std::string &str, const std::string &delimiter);
+std::string join(const std::vector<std::string> &parts, char delimiter);
 std::string join(const std::vector<std::string> &parts, const std::string &delimiter);
 std::string to_lower(const std::string &str);
 std::string to_upper(const std::string &str);
