bd808aa985a9e58a831f8db987b916b46f0e2c5d
abin <abin.z@outlook.com>
2026-01-26 00:10:00 +0800

    perf: 优化split函数

--- utils/src/string_utils.cpp
+++ utils/src/string_utils.cpp
@@ -9,23 +9,31 @@ std::vector<std::string> split(const std::string &str, char delimiter, bool skip
 std::vector<std::string> split(const std::string &str, const std::string &delimiter, bool skip_empty)
 {
   std::vector<std::string> tokens;
+
+  if (delimiter.empty())
+  {
+    if (!skip_empty || !str.empty()) tokens.emplace_back(str);
+    return tokens;
+  }
+
   std::string::size_type start = 0;
   std::string::size_type pos = 0;
+
   while ((pos = str.find(delimiter, start)) != std::string::npos)
   {
-    std::string token = str.substr(start, pos - start);
-    if (!skip_empty || !token.empty())
+    if (!skip_empty || pos != start)
     {
-      tokens.emplace_back(std::move(token));
+      tokens.emplace_back(str.substr(start, pos - start));
     }
     start = pos + delimiter.length();
   }
-  // 处理最后一部分
-  std::string last_token = str.substr(start);
-  if (!skip_empty || !last_token.empty())
+
+  if (!skip_empty || start < str.size())
   {
-    tokens.emplace_back(std::move(last_token));
+    tokens.emplace_back(str.substr(start));
   }
+
   return tokens;
 }
+
 }  // namespace stringutils
