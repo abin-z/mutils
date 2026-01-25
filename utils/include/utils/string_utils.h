d3449f558b48b1a131396dc7fe3c9f5510774032
abin <abin.z@outlook.com>
2026-01-25 23:57:32 +0800

    feat: 实现split函数

--- utils/include/utils/string_utils.h
+++ utils/include/utils/string_utils.h
@@ -16,8 +16,8 @@
 
 namespace stringutils
 {
-std::vector<std::string> split(const std::string &str, char delimiter);
-std::vector<std::string> split(const std::string &str, const std::string &delimiter);
+std::vector<std::string> split(const std::string &str, char delimiter, bool skip_empty = false);
+std::vector<std::string> split(const std::string &str, const std::string &delimiter, bool skip_empty = false);
 std::string join(const std::vector<std::string> &parts, char delimiter);
 std::string join(const std::vector<std::string> &parts, const std::string &delimiter);
 std::string to_lower(const std::string &str);
--- utils/src/string_utils.cpp
+++ utils/src/string_utils.cpp
@@ -2,5 +2,30 @@
 
 namespace stringutils
 {
-
+std::vector<std::string> split(const std::string &str, char delimiter, bool skip_empty)
+{
+  return split(str, std::string(1, delimiter), skip_empty);
+}
+std::vector<std::string> split(const std::string &str, const std::string &delimiter, bool skip_empty)
+{
+  std::vector<std::string> tokens;
+  std::string::size_type start = 0;
+  std::string::size_type pos = 0;
+  while ((pos = str.find(delimiter, start)) != std::string::npos)
+  {
+    std::string token = str.substr(start, pos - start);
+    if (!skip_empty || !token.empty())
+    {
+      tokens.emplace_back(std::move(token));
+    }
+    start = pos + delimiter.length();
+  }
+  // 处理最后一部分
+  std::string last_token = str.substr(start);
+  if (!skip_empty || !last_token.empty())
+  {
+    tokens.emplace_back(std::move(last_token));
+  }
+  return tokens;
+}
 }  // namespace stringutils
